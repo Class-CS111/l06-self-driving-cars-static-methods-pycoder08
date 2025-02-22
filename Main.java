7c17cfe08247f8b2748fec81a789ac88795dbc81
Muhammad Conn <126736146+pycoder08@users.noreply.github.com>
2025-02-22 13:00:09 -0800

    Update Main.java
    Wrote the desired function and labeled its components.

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 // When you are ready to have your work checked, change the value below from N to Y
-// ReadyForSubmission=N
-// YOUR NAME:
-// COLLABORATORS:
-// DATE:  
+// ReadyForSubmission=Y
+// YOUR NAME: Muhammad Conn
+// COLLABORATORS:N/A
+// DATE:  02/22/25
 
 public class Main
 {
@@ -10,9 +10,18 @@ public class Main
 	{
 		System.out.println("My thoughts on the article are:");
 		// Part 2: Invoking/calling method
-		
+		Main.printThoughts();
 	}
 	
 	// Part 1: Creating method definition
-	
+    
+    	//Method Header: Return type: Void, Method Identifier: printThoughts()
+	public static void printThoughts() 
+    {
+        //Method Body
+        System.out.println("1. The car should choose the most moral option regardless of the status of the person in question, choosing to spare the greatest number of people.");
+        System.out.println("2. As the article says, age, whether human or animal, and quantity of people need to be considered.");
+        System.out.println("3. The decision should be influenced by the public, but ultimately, there needs to be a standard set by an official regulatory body, just like all the other rules for self-driving cars.");
+        
+    }
 }
